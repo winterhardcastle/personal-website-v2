2372cd8c9978dc0ffd6854e69697a57a33de63f8
Winter Hardcastle <winter@hardcastle.io>
2023-09-12 18:46:56 -0400

    First Commit, Tailwind config and file structure, threejs install

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#121212",
+        white: "#F7F7F7",
+        reseda: "7B7554",
+        orange: "BA5624",
+      },
+    },
+  },
+  plugins: [],
+};
