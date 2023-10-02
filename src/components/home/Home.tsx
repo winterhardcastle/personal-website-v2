8436fc350d892ab15c10c061967057b103985725
Winter Hardcastle <winter@hardcastle.io>
2023-10-02 11:11:36 -0400

    Changed home styling

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,13 +3,16 @@ import SoftwareProjects from "../projects/SoftwareProjects";
 
 const Home = () => {
     return (
-        <>
         <div>
-            <h1 className="text-black font-bold text-8xl">WINTER HARDCASTLE</h1>
+        <div className="h-screen flex justify-around items-center">
+        <div className="pb-32">
+            <h1 className="text-black font-bold max-md:text-6xl text-8xl">WINTER HARDCASTLE</h1>
+            <p className="text-black text-2xl text-left">Software Developer and Maker</p>
+        </div>
         </div>
             <SoftwareProjects />
             <HardwareProjects />
-        </>
+        </div>
     )
 }
 
