8420f3073f6777e0e376e670105fd81fddb97890
Winter Hardcastle <winter@hardcastle.io>
2023-11-15 16:28:56 -0500

    Mobile compatibility changes to hardware projects and about me

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,10 +1,10 @@
 const About = () => {
     return (
         <div className="h-screen flex flex-col justify-center" id="aboutme">
-            <h1 className="pb-12 pl-[25vw] font-bold text-left text-5xl">About Me</h1>
-            <div className="flex justify-around">
-                <div className="w-1/2 flex gap-4 justify-left">
-                    <div className="md:text-left text-center">
+            <h1 className="pb-12 md:pl-[25vw] font-bold text-left max-md:text-4xl text-5xl">About Me</h1>
+            <div className="max-md:text-left flex justify-around">
+                <div className="md:w-1/2 flex gap-4 justify-left">
+                    <div className="text-left md:text-center">
                         <p className="py-4 text-sm md:text-lg">
                         My name is Winter Hardcastle, I'm a web developer and creative who is passionate about building great software.
                         I believe creativity is the catalyst for motivational energy, and I'm constantly striving to learn new things. I’m always looking for exciting opportunities, so don’t hesitate to reach out if you want to work together!
--- src/components/projects/HardwareProjects.tsx
+++ src/components/projects/HardwareProjects.tsx
@@ -27,24 +27,28 @@ const HardwareProjects = () => {
     }   
     return (
         <div className='pt-48 pb-12'>
-        <h1 className="pl-[25vw] font-bold text-left text-5xl">Designs and Hardware Projects</h1>
-        <div className="p-12 flex items-center justify-center">
-        <IconButton color="primary" onClick={() => {selectModel(modelNum - 1)}} size="large">
-            <ArrowBackIosNewIcon fontSize="inherit"/>
-        </IconButton>
-        <div className="w-[50vw] h-[75vh]">
-            <Canvas camera={{ fov: 90, near: 0.5, far: 750, position: [0, 0, 6]}}>
-                <OrbitControls enablePan= {false} enableZoom={false} />
-                <ambientLight color="#BA5624" intensity={.5}/>
-                <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr" />
-                <directionalLight position={[0, 0, 5]} intensity={1}/>
-                {models[modelNum]}
-            </Canvas>
-        </div>
-        <IconButton color="primary" onClick={() => {selectModel(modelNum + 1)}} size="large">
-            <ArrowForwardIosIcon fontSize="inherit"/>
-        </IconButton>
-        </div>
+            <h1 className="md:pl-[25vw] font-bold text-left max-md:text-4xl text-5xl">Designs and Hardware Projects</h1>
+            <div className="md:p-12 flex items-center justify-center">
+                <div className="max-md:hidden">
+                    <IconButton color="primary" onClick={() => {selectModel(modelNum - 1)}} size="large">
+                        <ArrowBackIosNewIcon fontSize="inherit"/>
+                    </IconButton>
+                </div>
+                <div className="md:w-[50vw] h-[75vh]">
+                    <Canvas camera={{ fov: 90, near: 0.5, far: 750, position: [0, 0, 6]}}>
+                        <OrbitControls enablePan= {false} enableZoom={false} />
+                        <ambientLight color="#BA5624" intensity={.5}/>
+                        <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr" />
+                        <directionalLight position={[0, 0, 5]} intensity={1}/>
+                        {models[modelNum]}
+                    </Canvas>
+                </div>
+                <div className="max-md:hidden">
+                    <IconButton color="primary" onClick={() => {selectModel(modelNum + 1)}} size="large">
+                        <ArrowForwardIosIcon fontSize="inherit"/>
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
