2de5c2958ba7f9b62efa344e886ffa05b63ecf44
Winter Hardcastle <winter@hardcastle.io>
2023-11-28 13:20:45 -0500

    Added swipeable on mobile for hardware projects

--- src/components/projects/HardwareProjects.tsx
+++ src/components/projects/HardwareProjects.tsx
@@ -5,6 +5,8 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { Canvas, useFrame} from "@react-three/fiber";
 import {OrbitControls, PresentationControls, AccumulativeShadows, RandomizedLight, Environment} from "@react-three/drei"
 import {Speaker, BikeBrake, BucklingSpring, ElektraBoilerCap, ElektraSteamKnob, FilmCanister, Slavboard, KeyboardMounts} from './files'
+import { useSwipeable } from "react-swipeable";
+
 
 const models = [<Speaker />, <BucklingSpring />, <BikeBrake />, <FilmCanister />, <Slavboard />, <KeyboardMounts />, <ElektraBoilerCap/>, <ElektraSteamKnob />]
 
@@ -24,11 +26,17 @@ const HardwareProjects = () => {
             setModelNum(num)
             return modelNum
         }
-    }   
+    }
+
+    const mobileSwipe = useSwipeable({
+        onSwipedLeft: () => {selectModel(modelNum - 1)},
+        onSwipedRight: () => {selectModel(modelNum + 1)}
+    }) 
+
     return (
         <div className='pt-48 pb-12'>
             <h1 className="md:pl-[25vw] font-bold text-left max-md:text-4xl text-5xl">Designs and Hardware Projects</h1>
-            <div className="md:p-12 flex items-center justify-center">
+            <div {...mobileSwipe} className="md:p-12 flex items-center justify-center">
                 <div className="max-md:hidden">
                     <IconButton color="primary" onClick={() => {selectModel(modelNum - 1)}} size="large">
                         <ArrowBackIosNewIcon fontSize="inherit"/>
